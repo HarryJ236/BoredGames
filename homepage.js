735689cd6e0252878728f64f5f7857e3361713fc
HarryJ <Harryjackson236@gmail.com>
2025-04-29 22:29:37 -0400

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -1,9 +1,7 @@
-// Navigation Functionality
+// Homepage functions
 function navigateTo(link) {
   window.location.href = link;
 }
-
-// Highlight Active Navigation Link
 document.addEventListener("DOMContentLoaded", () => {
   const currentPath = window.location.pathname;
   const navLinks = document.querySelectorAll(".nav-link");
