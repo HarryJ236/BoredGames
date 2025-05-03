640609f11ae876f757260ea99c5468a54bc6c1b7
HarryJ <Harryjackson236@gmail.com>
2025-05-03 19:12:06 -0400

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,14 @@
+// firebase-init.js (all pages will refer to this for connection to firebase)
+const firebaseConfig = {
+  apiKey: "AIzaSyCuekWtRk9TOUuchwgQ_qPZkc_BbJ0rQmQ",
+  authDomain: "boredgames-website.firebaseapp.com",
+  projectId: "boredgames-website",
+  storageBucket: "boredgames-website.firebasestorage.app",
+  messagingSenderId: "728925547202",
+  appId: "1:728925547202:web:f5a0da6d3df1ab66a7695b",
+  measurementId: "G-083HCSVHRF"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
